682bedecb3db80df5da1599f158dd179bebf26c6
gokul <gokulakrishnan.312@gmail.com>
2020-07-29 13:25:02 +0530

    Updates

---
+++ precache-manifest.49bd7701d477411109def9b7d2a3c0cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50cd2cd66508640bc3c76c031f02ea2b",
+    "url": "/oauthproject/index.html"
+  },
+  {
+    "revision": "501efebb0f5148a5f4f2",
+    "url": "/oauthproject/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "836e1a37f18b1f6bfeff",
+    "url": "/oauthproject/static/js/2.9db8f6ad.chunk.js"
+  },
+  {
+    "revision": "dd139775825dc4cf08a37bfc7c566858",
+    "url": "/oauthproject/static/js/2.9db8f6ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "501efebb0f5148a5f4f2",
+    "url": "/oauthproject/static/js/main.b767d486.chunk.js"
+  },
+  {
+    "revision": "14f765e67af86cdd58ca",
+    "url": "/oauthproject/static/js/runtime-main.9952dbd6.js"
+  }
+]);
